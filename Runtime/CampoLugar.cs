1881e944a6fd49585e61065e80010eabb0f1d21f
Ging1991 <carloscaballeromorel@gmail.com>
2026-01-11 00:41:20 -0300

    Refactor de campo lugar

---
+++ Runtime/CampoLugar.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Bounds.Modulos.Duelo.Fisicas {
+
+	public class CampoLugar : MonoBehaviour, IDropHandler {
+
+		public GameObject carta;
+		public int jugador;
+		public int lugarID;
+		public ICampoLugarControlador controlador;
+
+		void OnMouseDown() {
+			if (carta == null) {
+				controlador.LugarPresionado(gameObject);
+			}
+		}
+
+		public void OnDrop(PointerEventData eventData) {
+			if (carta == null) {
+				controlador.LugarSoltado(gameObject);
+			}
+		}
+
+	}
+
+}
---
+++ Runtime/ICampoLugarControlador.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Bounds.Modulos.Duelo.Fisicas {
+
+	public interface ICampoLugarControlador {
+
+		void LugarPresionado(GameObject objeto);
+
+		void LugarSoltado(GameObject objeto);
+
+	}
+
+}
